a68939b1cec3224365bbf4211e04d72841978f10
RC <aarsheestar@gmail.com>
2025-08-11 20:04:29 +0545

    Initial

--- frontend1/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { ClerkProvider } from '@clerk/clerk-react';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx'
-import './index.css'
-
-createRoot(document.getElementById("root")!).render(<App />);
